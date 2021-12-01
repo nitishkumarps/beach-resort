5de45d247fad3357a34a4e89497bf2990d914b7f
Reasy <nitishkumarps997@gmail.com>
2021-12-02 00:41:19 +0530

    Number issue fix

--- src/components/WhatsappClick.js
+++ src/components/WhatsappClick.js
@@ -5,7 +5,7 @@ export default function WhatsappClick() {
   return (
     <div className="whatsapp-click">
       <a
-        href="https://wa.me/9632916893"
+        href="https://wa.me/919632916893"
         target="_blank"
         rel="noopener noreferrer"
       >
